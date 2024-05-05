03db6926f1bb2255dc7438c245376ba060824c26
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2024-05-05 20:57:09 +0100

    create the basic gameloop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 16.67;
+const FLY_VELOCITY: f32 = -2.5;
+const FALL_VELOCITY: f32 = 0.4;
+
 enum GameMode {
     Menu,
     Playing,
@@ -7,19 +13,50 @@ enum GameMode {
 }
 struct State {
     mode: GameMode,
+    frame_time: f32,
+    player: Player,
+    obstacle: Obstacle,
+    score: i32,
 }
 impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
+            frame_time: 0.0,
+            player: Player::new(5, 25),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.fly();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to fly.");
+        ctx.print(0, 1, format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+            self.mode = GameMode::End;
+        }
     }
     fn restart(&mut self) {
         self.mode = GameMode::Playing;
+        self.frame_time = 0.0;
+        self.player = Player::new(5, 25);
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.score = 0
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
@@ -40,6 +77,7 @@ impl State {
             5,
             "You are dead! well i guess you never were alive to begin with...",
         );
+        ctx.print_centered(6, format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
         if let Some(key) = ctx.key {
@@ -60,6 +98,76 @@ impl GameState for State {
         }
     }
 }
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += FALL_VELOCITY;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+    fn fly(&mut self) {
+        self.velocity = FLY_VELOCITY;
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+        //render top section
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, GREEN, BLACK, to_cp437('|'));
+        }
+        //render bottom section
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, GREEN, BLACK, to_cp437('|'));
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = self.x == player.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
+    }
+}
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Ghost")
